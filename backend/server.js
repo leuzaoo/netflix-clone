0cf9aff6eeb64f63231d0dddb09a787df698ddae
leuzaoo <dev.lcostaoliveira@gmail.com>
2024-08-29 19:49:40 -0300

    Created environment variables to app

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,15 @@
 import express from "express";
+
+import { connectDB } from "./config/dbConnection.js";
+import { ENV_VARS } from "./config/envVariables.js";
 import authRoutes from "./routes/auth.router.js";
 
 const app = express();
-const PORT = 5000;
+const PORT = ENV_VARS.PORT;
 
 app.use("/api/v1/auth", authRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
+  connectDB();
 });

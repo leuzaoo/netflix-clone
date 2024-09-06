1952379391e1e31ae7443d6bf9397fa618510f60
leuzaoo <dev.lcostaoliveira@gmail.com>
2024-09-06 12:13:20 -0300

    Added new controllers to access endpoints

--- backend/controllers/movie.controller.js
+++ backend/controllers/movie.controller.js
@@ -35,3 +35,52 @@ export async function getMovieTrailers(req, res) {
       .json({ success: false, message: "Erro no servidor interno." });
   }
 }
+
+export async function getMovieDetails(req, res) {
+  const { id } = req.params;
+  try {
+    const data = await fetchFromTMDB(
+      `https://api.themoviedb.org/3/movie/${id}?language=en-US`
+    );
+
+    res.json({ success: true, content: data });
+  } catch (error) {
+    if (error.message.includes("404")) {
+      return res.status(404).send(null);
+    }
+
+    res
+      .status(500)
+      .json({ success: false, message: "Erro no servidor interno." });
+  }
+}
+
+export async function getSimilarMovies(req, res) {
+  const { id } = req.params;
+  try {
+    const data = await fetchFromTMDB(
+      `https://api.themoviedb.org/3/movie/${id}/similar?language=pt-BR&page=1`
+    );
+
+    res.status(200).json({ success: true, similarMovies: data.results });
+  } catch (error) {
+    res
+      .status(500)
+      .json({ success: false, message: "Erro no servidor interno." });
+  }
+}
+
+export async function getMoviesByCategory(req, res) {
+  const { category } = req.params;
+  try {
+    const data = await fetchFromTMDB(
+      `https://api.themoviedb.org/3/movie/${category}?language=pt-BR&page=1`
+    );
+
+    res.status(200).json({ success: true, content: data.results });
+  } catch (error) {
+    res
+      .status(500)
+      .json({ success: false, message: "Erro no servidor interno." });
+  }
+}
--- backend/routes/movie.route.js
+++ backend/routes/movie.route.js
@@ -1,5 +1,8 @@
 import {
+  getMovieDetails,
+  getMoviesByCategory,
   getMovieTrailers,
+  getSimilarMovies,
   getTrendingMovie,
 } from "../controllers/movie.controller.js";
 import express from "express";
@@ -7,6 +10,9 @@ import express from "express";
 const router = express.Router();
 
 router.get("/trending", getTrendingMovie);
+router.get("/:id/details", getMovieDetails);
+router.get("/:id/similar", getSimilarMovies);
 router.get("/:id/trailers", getMovieTrailers);
+router.get("/:category", getMoviesByCategory);
 
 export default router;

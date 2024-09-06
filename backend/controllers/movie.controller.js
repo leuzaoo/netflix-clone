ffa3fee2c8299f4279599e667f011d46742d15d8
leuzaoo <dev.lcostaoliveira@gmail.com>
2024-09-06 11:03:07 -0300

    Added function to fetch movies trailers

--- backend/controllers/movie.controller.js
+++ backend/controllers/movie.controller.js
@@ -16,3 +16,22 @@ export async function getTrendingMovie(req, res) {
       .json({ success: false, message: "Erro no servidor interno." });
   }
 }
+
+export async function getMovieTrailers(req, res) {
+  const { id } = req.params;
+
+  try {
+    const data = await fetchFromTMDB(
+      `https://api.themoviedb.org/3/movie/${id}/videos?language=en-US`
+    );
+    res.json({ success: true, trailers: data.results });
+  } catch (error) {
+    if (error.message.includes("404")) {
+      return res.status(404).send(null);
+    }
+
+    res
+      .status(500)
+      .json({ success: false, message: "Erro no servidor interno." });
+  }
+}
--- backend/routes/movie.route.js
+++ backend/routes/movie.route.js
@@ -1,8 +1,12 @@
-import { getTrendingMovie } from "../controllers/movie.controller.js";
+import {
+  getMovieTrailers,
+  getTrendingMovie,
+} from "../controllers/movie.controller.js";
 import express from "express";
 
 const router = express.Router();
 
 router.get("/trending", getTrendingMovie);
+router.get("/:id/trailers", getMovieTrailers);
 
 export default router;

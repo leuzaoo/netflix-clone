fe9bdc966941260527af3e96ca9e987c7484e077
leuzaoo <dev.lcostaoliveira@gmail.com>
2024-09-06 09:44:59 -0300

    Updated name file

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,8 @@
 import express from "express";
 
 import { connectDB } from "./config/dbConnection.js";
-import authRoutes from "./routes/auth.router.js";
+import movieRoutes from "./routes/movie.route.js";
+import authRoutes from "./routes/auth.route.js";
 import { ENV_VARS } from "./config/envVars.js";
 
 const app = express();
@@ -11,6 +12,7 @@ const PORT = ENV_VARS.PORT;
 app.use(express.json()); // will allow us to parse req.body
 
 app.use("/api/v1/auth", authRoutes);
+app.use("/api/v1/movie", movieRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);

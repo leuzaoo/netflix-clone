a45bfe32f262e3c13e6adf9df2e8523a13d70968
leuzaoo <dev.lcostaoliveira@gmail.com>
2024-09-09 14:04:29 -0300

    Created new route to search movies, person or tv shows

---
+++ backend/controllers/search.controller.js
@@ -0,0 +1,38 @@
+import fetchFromTMDB from "../services/tmdb.service.js";
+import { User } from "../models/user.model.js";
+
+export async function searchPerson(req, res) {
+  const { query } = req.params;
+  try {
+    const response = await fetchFromTMDB(
+      `https://api.themoviedb.org/3/search/person?query=${query}&include_adult=false&language=en-US&page=1`
+    );
+
+    if (response.results.length === 0) {
+      return res.status(404).send(null);
+    }
+
+    await User.findByIdAndUpdate(req.user._id, {
+      $push: {
+        searchHistory: {
+          id: response.results[0].id,
+          image: response.results[0].profile_path,
+          title: response.results[0].name,
+          searchType: "person",
+          createdAt: new Date(),
+        },
+      },
+    });
+
+    res.status(200).json({ success: true, content: response.results });
+  } catch (error) {
+    console.log("Erro no controlador 'searchPerson':", error.message);
+    res
+      .status(500)
+      .json({ success: false, message: "Erro no servidor interno." });
+  }
+}
+
+export async function searchMovie(req, res) {}
+
+export async function searchTv(req, res) {}

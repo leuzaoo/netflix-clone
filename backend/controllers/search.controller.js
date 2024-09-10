4b2ecdefc818d670d16eec36d2a4da57c0b97e00
leuzaoo <dev.lcostaoliveira@gmail.com>
2024-09-09 23:38:05 -0300

    Created functions to fetch datas by query typed by user

--- backend/controllers/search.controller.js
+++ backend/controllers/search.controller.js
@@ -5,7 +5,7 @@ export async function searchPerson(req, res) {
   const { query } = req.params;
   try {
     const response = await fetchFromTMDB(
-      `https://api.themoviedb.org/3/search/person?query=${query}&include_adult=false&language=en-US&page=1`
+      `https://api.themoviedb.org/3/search/person?query=${query}&include_adult=false&language=pt-BR&page=1`
     );
 
     if (response.results.length === 0) {
@@ -33,6 +33,68 @@ export async function searchPerson(req, res) {
   }
 }
 
-export async function searchMovie(req, res) {}
+export async function searchMovie(req, res) {
+  const { query } = req.params;
 
-export async function searchTv(req, res) {}
+  try {
+    const response = await fetchFromTMDB(
+      `https://api.themoviedb.org/3/search/movie?query=${query}&include_adult=false&language=pt-BR&page=1`
+    );
+
+    if (response.results.length === 0) {
+      return res.status(404).send(null);
+    }
+
+    await User.findByIdAndUpdate(req.user._id, {
+      $push: {
+        searchHistory: {
+          id: response.results[0].id,
+          image: response.results[0].poster_path,
+          title: response.results[0].title,
+          searchType: "movie",
+          createdAt: new Date(),
+        },
+      },
+    });
+
+    res.status(200).json({ success: true, content: response.results });
+  } catch (error) {
+    console.log("Erro no controlador 'searchMovie':", error.message);
+    res
+      .status(500)
+      .json({ success: false, message: "Erro no servidor interno." });
+  }
+}
+
+export async function searchTv(req, res) {
+  const { query } = req.params;
+
+  try {
+    const response = await fetchFromTMDB(
+      `https://api.themoviedb.org/3/search/tv?query=${query}&include_adult=false&language=pt-BR&page=1`
+    );
+
+    if (response.results.length === 0) {
+      return res.status(404).send(null);
+    }
+
+    await User.findByIdAndUpdate(req.user._id, {
+      $push: {
+        searchHistory: {
+          id: response.results[0].id,
+          image: response.results[0].poster_path,
+          title: response.results[0].name,
+          searchType: "tv",
+          createdAt: new Date(),
+        },
+      },
+    });
+
+    res.json({ success: true, content: response.results });
+  } catch (error) {
+    console.log("Erro no controlador 'searchTv':", error.message);
+    res
+      .status(500)
+      .json({ success: false, message: "Erro no servidor interno." });
+  }
+}

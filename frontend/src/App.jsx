b7fd31532fed90553fd3243a42a431b31ebcee61
leuzaoo <dev.lcostaoliveira@gmail.com>
2024-09-11 21:48:47 -0300

    Updates

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,8 @@
 import { Route, Routes } from "react-router-dom";
 
-import Home from "./pages/Home";
-import Login from "./pages/Login";
+import Home from "./pages/home/Home";
 import SignUp from "./pages/SignUp";
+import Login from "./pages/Login";
 
 function App() {
   return (

780b0deed7e89f1355d9e4b57e4af0187b46be43
leuzaoo <dev.lcostaoliveira@gmail.com>
2024-09-12 12:38:56 -0300

    Updated pages and added footer component to all app

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,14 +3,18 @@ import { Route, Routes } from "react-router-dom";
 import Home from "./pages/home/Home";
 import SignUp from "./pages/SignUp";
 import Login from "./pages/Login";
+import Footer from "./components/Footer";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/login" element={<Login />} />
-      <Route path="/signup" element={<SignUp />} />
-    </Routes>
+    <>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<SignUp />} />
+      </Routes>
+      <Footer />
+    </>
   );
 }
 
--- frontend/src/pages/home/AuthScreen.jsx
+++ frontend/src/pages/home/AuthScreen.jsx
@@ -83,6 +83,126 @@ const AuthScreen = () => {
           </div>
         </div>
       </section>
+
+      <section>
+        <h1 className="hidden">Offline</h1>
+        <div className="py-10 bg-black text-white">
+          <div className="flex max-w-6xl mx-auto items-center justify-center md:flex-row flex-col-reverse px-4 md:px-2">
+            {/* left side */}
+            <div className="flex-1 relative">
+              <div className="relative">
+                <img
+                  src="/stranger-things-lg.png"
+                  alt="Stranger Things img"
+                  className="mt-4"
+                />
+
+                <div
+                  className="flex items-center gap-2 absolute bottom-5 left-1/2 -translate-x-1/2 bg-black
+              w-3/4 lg:w-1/2 h-24 border border-slate-500 rounded-md px-2
+              "
+                >
+                  <img
+                    src="/stranger-things-sm.png"
+                    alt="image"
+                    className="h-full"
+                  />
+                  <div className=" flex justify-between items-center w-full">
+                    <div className="flex flex-col gap-0">
+                      <span className="text-md lg:text-lg font-bold">
+                        Stranger Things
+                      </span>
+                      <span className="text-sm text-blue-500">
+                        Downloading...
+                      </span>
+                    </div>
+
+                    <img src="/download-icon.gif" alt="" className="h-12" />
+                  </div>
+                </div>
+              </div>
+            </div>
+            {/* right side */}
+
+            <div className="flex-1 md:text-left text-center">
+              <h2 className="text-4xl md:text-5xl font-extrabold mb-4 text-balance">
+                Baixe séries para assistir offline
+              </h2>
+              <p className="text-lg md:text-xl">
+                Salve seus títulos favoritos e sempre tenha algo para assistir.
+              </p>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <div className="h-2 w-full bg-[#232323]" aria-hidden="true" />
+
+      <section>
+        <h1 className="hidden">Aparelhos</h1>
+        <div className="py-10 bg-black text-white">
+          <div className="flex max-w-6xl mx-auto items-center justify-center md:flex-row flex-col px-4 md:px-2">
+            {/* left side */}
+            <div className="flex-1 text-center md:text-left">
+              <h2 className="text-4xl md:text-5xl font-extrabold mb-4">
+                Assista onde quiser
+              </h2>
+              <p className="text-lg md:text-xl">
+                Assista a quantos filmes e séries quiser no celular, tablet,
+                laptop e TV.
+              </p>
+            </div>
+
+            {/* right side */}
+            <div className="flex-1 relative overflow-hidden">
+              <img
+                src="/device-pile.png"
+                alt="Device image"
+                className="mt-4 z-20 relative"
+              />
+              <video
+                className="absolute top-2 left-1/2 -translate-x-1/2  h-4/6 z-10
+               max-w-[63%] 
+              "
+                playsInline
+                autoPlay={true}
+                muted
+                loop
+              >
+                <source src="/video-devices.m4v" type="video/mp4" />
+              </video>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <div className="h-2 w-full bg-[#232323]" aria-hidden="true" />
+
+      <section>
+        <h1 className="hidden">Para crianças</h1>
+        <div className="py-10 bg-black text-white">
+          <div
+            className="flex max-w-6xl mx-auto items-center justify-center flex-col-reverse md:flex-row
+           px-4 md:px-2
+        "
+          >
+            {/* left */}
+            <div className="flex-1 relative">
+              <img src="/kids.png" alt="Enjoy on your TV" className="mt-4" />
+            </div>
+            {/* right */}
+            <div className="flex-1 text-center md:text-left">
+              <h2 className="text-4xl md:text-5xl font-extrabold mb-4">
+                Crie perfil para crianças
+              </h2>
+              <p className="text-lg md:text-xl">
+                Deixe as crianças se aventurarem com seus personagens favoritos
+                em um espaço feito só para elas, sem pagar a mais por isso.
+              </p>
+            </div>
+          </div>
+        </div>
+      </section>
     </div>
   );
 };

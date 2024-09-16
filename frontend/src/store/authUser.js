0e30829b189a54da2875450328bd8be90bfc3576
leuzaoo <dev.lcostaoliveira@gmail.com>
2024-09-16 09:56:50 -0300

    Added signup logic and logout functions

--- frontend/src/store/authUser.js
+++ frontend/src/store/authUser.js
@@ -5,18 +5,39 @@ import axios from "axios";
 export const useAuthStore = create((set) => ({
   user: null,
   isSigningUp: false,
+  isCheckingAuth: true,
+  isLoggingOut: false,
   signup: async (credentials) => {
     set({ isSigningUp: true });
     try {
       const response = await axios.post("/api/v1/auth/signup", credentials);
       set({ user: response.data.user, isSigningUp: false });
-      toast.success("Você entrou na sua conta.");
+      toast.success("Conta criada com sucesso.");
     } catch (error) {
       toast.error(error.response.data.message || "Um erro foi encontrado.");
       set({ isSigningUp: false, user: null });
     }
   },
   login: async () => {},
-  logout: async () => {},
-  authCheck: async () => {},
+  logout: async () => {
+    try {
+      await axios.post("/api/v1/auth/logout");
+      set({ user: null, isLoggingOut: false });
+      toast.success("Você saiu da sua conta.");
+    } catch (error) {
+      set({ isLoggingOut: false });
+      toast.error(
+        error.response.data.message || "Erro ao tentar sair da conta."
+      );
+    }
+  },
+  authCheck: async () => {
+    try {
+      const response = await axios.get("/api/v1/auth/authCheck");
+      set({ user: response.data.user, isCheckingAuth: false });
+    } catch (error) {
+      set({ isCheckingAuth: false, user: null });
+      toast.error(error.response.data.message || "Foi encontrado um erro.");
+    }
+  },
 }));

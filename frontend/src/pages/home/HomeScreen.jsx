39babcbe4ab0ec7d2d5c8334f066c1fa8f594f3f
leuzaoo <dev.lcostaoliveira@gmail.com>
2024-09-17 10:02:04 -0300

    Updated homescreen

--- frontend/src/pages/home/HomeScreen.jsx
+++ frontend/src/pages/home/HomeScreen.jsx
@@ -1,13 +1,12 @@
-import { useAuthStore } from "../../store/authUser.js";
+import Navbar from "../../components/Navbar.jsx";
 
 const HomeScreen = () => {
-  const { logout } = useAuthStore();
-
   return (
-    <div>
-      teste
-      <button onClick={logout}>Sair</button>
-    </div>
+    <>
+      <div className="relative h-screen text-white bg-black">
+        <Navbar />
+      </div>
+    </>
   );
 };
 

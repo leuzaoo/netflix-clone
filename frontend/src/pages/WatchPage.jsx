f50d4538d591b074d32be81d98aed3047b77ffc9
leuzaoo <dev.lcostaoliveira@gmail.com>
2024-09-20 10:10:07 -0300

    Added more funcionalities to app

--- frontend/src/pages/WatchPage.jsx
+++ frontend/src/pages/WatchPage.jsx
@@ -1,7 +1,12 @@
-import axios from "axios";
-import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { ORIGINAL_IMG_BASE_URL } from "../utils/contants";
+import { formatReleaseDate } from "../utils/dateFunction";
+import { ChevronLeft, ChevronRight } from "lucide-react";
 import { useContentStore } from "../store/content";
+import { useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+import Navbar from "../components/Navbar";
+import ReactPlayer from "react-player";
+import axios from "axios";
 
 const WatchPage = () => {
   const { id } = useParams();
@@ -61,11 +66,102 @@ const WatchPage = () => {
     getContentDetails();
   }, [contentType, id]);
 
-  console.log("similar:", similarContent);
-  console.log("trailers:", trailers);
-  console.log("details:", content);
+  const handleNext = () => {
+    if (currentTrailerIdx < trailers.length - 1) {
+      setCurrentTrailerIdx(currentTrailerIdx + 1);
+    }
+  };
 
-  return <div>WatchPage</div>;
+  const handlePrev = () => {
+    if (currentTrailerIdx > 0) {
+      setCurrentTrailerIdx(currentTrailerIdx - 1);
+    }
+  };
+
+  return (
+    <div className="bg-black justify-between items-center">
+      <div className="mx-auto container px-4 py-8 h-full">
+        <Navbar />
+
+        {trailers.length > 0 && (
+          <div className="flex justify-between items-center mb-4">
+            <button
+              className={`
+							bg-gray-500/70 hover:bg-gray-500 text-white py-2 px-4 rounded ${
+                currentTrailerIdx === 0 ? "opacity-50 cursor-not-allowed" : ""
+              }}
+							`}
+              disabled={currentTrailerIdx === 0}
+              onClick={handlePrev}
+            >
+              <ChevronLeft size={24} />
+            </button>
+
+            <button
+              className={`
+							bg-gray-500/70 hover:bg-gray-500 text-white py-2 px-4 rounded ${
+                currentTrailerIdx === trailers.length - 1
+                  ? "opacity-50 cursor-not-allowed "
+                  : ""
+              }}
+							`}
+              disabled={currentTrailerIdx === trailers.length - 1}
+              onClick={handleNext}
+            >
+              <ChevronRight size={24} />
+            </button>
+          </div>
+        )}
+
+        <div className="aspect-video mb-8 p-2 sm:px-10 md:px-32">
+          {trailers.length > 0 && (
+            <ReactPlayer
+              controls={true}
+              width={"100%"}
+              height={"60vh"}
+              className="mx-auto overflow-hidden rounded-lg"
+              url={`https://www.youtube.com/watch?v=${trailers[currentTrailerIdx].key}`}
+            />
+          )}
+
+          {trailers?.length === 0 && (
+            <h2 className="text-xl text-center mt-5">
+              Não há trailers disponível para este título{" "}
+              <span className="font-bold text-red-600">
+                {content?.title || content?.name}
+              </span>{" "}
+            </h2>
+          )}
+        </div>
+
+        <div className="flex flex-col md:flex-row items-center justify-between gap-20 max-w-6xl mx-auto">
+          <div className="mb-4 md:mb-0 text-white">
+            <h2 className="text-5xl font-bold text-balance">
+              {content?.title || content?.name}
+            </h2>
+
+            <p className="mt-2 text-lg">
+              {formatReleaseDate(
+                content?.release_date || content?.first_air_date
+              )}{" "}
+              |{" "}
+              {content?.adult ? (
+                <img src="/18yo.png" className="size-8" />
+              ) : (
+                <img src="/10yo.png" className="size-8" />
+              )}{" "}
+            </p>
+            <p className="mt-4 text-lg">{content?.overview}</p>
+          </div>
+          <img
+            src={ORIGINAL_IMG_BASE_URL + content?.poster_path}
+            alt="Poster image"
+            className="max-h-[600px] rounded-md"
+          />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default WatchPage;
